de8c311a9dca56ad4a09a6af1d90af61bd24c164
MohithMadugula <mohith.madugula@gmail.com>
2026-07-17 21:17:57 +0530

    Users Entity class was created

--- src/main/java/com/expensetracker/Model/User.java
+++ src/main/java/com/expensetracker/Model/User.java
@@ -1,4 +1,49 @@
 package com.expensetracker.Model;
 
+import com.expensetracker.Enums.AccountStatus;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name="users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="user_id")
+    private Long userId;
+
+    @Column(name="username", unique=true, nullable=false,length=30)
+    private String username;
+
+    @Email
+    @Column(name = "email", unique=true, nullable=false,length=100)
+    private String email;
+
+    @Column(name = "password", nullable=false,length=255)
+    private String password;
+
+    @Column(name = "phone_number", unique=true, length=15)
+    private String phoneNumber;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name= "status", nullable=false)
+    private AccountStatus status;
+
+    @Column(name="created_at", nullable=false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable=false)
+    private LocalDateTime updatedAt;
+
 }
